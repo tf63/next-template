9f40c98eb38aa939e28d595693fafa9f04f8313c
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-15 17:00:39 +0900

    [feat] update plopfile

--- plopfile.js
+++ plopfile.js
@@ -6,34 +6,34 @@ module.exports = function (plop) {
             {
                 type: 'input',
                 name: 'path',
-                default: 'components/',
-                message: 'component path (e.g. components/):',
+                default: '',
+                message: 'Please enter the directory name following components/:',
             },
             {
                 type: 'input',
                 name: 'name',
-                message: 'component name:',
+                message: 'Please enter the component name:',
             },
         ],
         actions: [
             {
                 type: 'add',
-                path: '{{path}}/{{kebabCase name}}/index.tsx',
+                path: 'components/{{path}}/{{kebabCase name}}/index.tsx',
                 templateFile: 'templates/components/index.tsx.hbs',
             },
             {
                 type: 'add',
-                path: '{{path}}/{{kebabCase name}}/{{kebabCase name}}.tsx',
+                path: 'components/{{path}}/{{kebabCase name}}/{{kebabCase name}}.tsx',
                 templateFile: 'templates/components/component.tsx.hbs',
             },
             {
                 type: 'add',
-                path: '{{path}}/{{kebabCase name}}/{{kebabCase name}}.spec.tsx',
+                path: 'components/{{path}}/{{kebabCase name}}/{{kebabCase name}}.spec.tsx',
                 templateFile: 'templates/components/component.spec.tsx.hbs',
             },
             {
                 type: 'add',
-                path: '{{path}}/{{kebabCase name}}/{{kebabCase name}}.stories.tsx',
+                path: 'components/{{path}}/{{kebabCase name}}/{{kebabCase name}}.stories.tsx',
                 templateFile: 'templates/components/component.stories.tsx.hbs',
             },
         ],
