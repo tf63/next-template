c028dca6e538cf1f9b23fd6ed62f2f5b593463bf
tf63 <74246282+tf63@users.noreply.github.com>
2024-07-16 12:21:48 +0900

    [feat] change tailwind ext

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+const config = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './components/**/*.{js,ts,jsx,tsx,mdx}',
+        './app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            keyframes: {
+                blink: {
+                    '0%, 100%': { opacity: '1' },
+                    '50%': { opacity: '0' },
+                },
+            },
+            animation: {
+                blink: 'blink 1s infinite',
+            },
+        },
+    },
+    plugins: [],
+}
+
+module.exports = config
